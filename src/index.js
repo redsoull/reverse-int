bfb04a3bbe742d1601800185c3eae6b19a2990a1
Volha Koval <volha.koval@mapbox.com>
2021-08-23 18:23:32 +0300

    init: create imdex.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let num=Math.abs(n).toString();
+  let result = '';
+  for (let i = 0; i < num.length; i +=1) {
+    result = `${num[i]}${result}`;
+  }
+  return result;
 }
